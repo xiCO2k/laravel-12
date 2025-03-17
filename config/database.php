521755e36ead40e9abf967a0ea0a91b5e9390aac
Francisco Madeira <xico2k@gmail.com>
2025-03-17 11:45:20 +0000

    add scheme

--- config/database.php
+++ config/database.php
@@ -158,6 +158,7 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+            'schema' => env('REDIS_SCHEME', 'tcp'),
         ],
 
         'cache' => [
